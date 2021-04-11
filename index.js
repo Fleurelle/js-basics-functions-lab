8ffa75b853c4a89fd8a8e77391f97fd99e876a65
Rose <r.d.jeanlouis01@gmail.com>
2021-04-10 20:53:46 -0400

    commit tree 95c985b9bd42a2be95d26e27ebc0f5e5fe18d5fa

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+//Return distance in blocks 
+function distanceFromHqInBlocks(distance){
+    const hQdist = 42 
+    if (distance >=42){        
+        return distance - hQdist; 
+    } else{
+        return hQdist - distance;
+    }
+}
+
+//Calculates distances from HQ in feet
+function distanceFromHqInFeet(totalDist){
+    return distanceFromHqInBlocks(totalDist)*264;
+    // let distInFt = totalDist*264;
+    // return distInFt;
+  }
+
+  //Return distances travelled in feet
+function distanceTravelledInFeet(startBlock, endBlock){
+    if (startBlock <= endBlock){
+        return (endBlock - startBlock)*264;
+    } else {
+        return (startBlock - endBlock)*264;
+    }
+}
+
+function calculatesFarePrice (startBlock, endBlock){
+   const distance = distanceTravelledInFeet (startBlock, endBlock);
+    if (distance <=400){
+        return 0;
+    } else if (distance <=2000 && distance >= 400){
+        return 0.02*(distance - 400);
+    } else if ( distance >= 2000 && distance <= 2500){
+        return 25;
+    } else {
+        return ("cannot travel that far")
+    }
+}
